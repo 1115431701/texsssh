a985b81c72e8790f955b88c78a1119a52e61b8ed
kang <1115431701.com>
2022-07-09 20:33:13 +0800

    测试 github

---
+++ src/main/java/atguigu/testssh/ssh.java
@@ -0,0 +1,10 @@
+package atguigu.testssh;
+
+public class ssh {
+    public static void main(String[] args) {
+        System.out.println("github");
+        System.out.println("github");
+        System.out.println("github");
+        System.out.println("github");
+    }
+}
